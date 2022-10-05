5a4ed43e913a1fa71221116a2b911baed65d4711
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-05 19:27:53 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+//
+// This program displays, "Hello, World!"
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("\nDone.");
+  }
+}
