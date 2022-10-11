beb13dfae3f6ff2a58d83329bf0bcd8eca4bc58d
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-11 18:18:34 +0000

    fixed whitespace 928

--- main.cs
+++ main.cs
@@ -6,7 +6,7 @@ using System;
 
 class Program
 {
-    public static void Main (string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
         Console.WriteLine("\nDone.");
